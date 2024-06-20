33b1cdf4a779fe4b3457090720c9885c2931197b
xo482 <ksh990409@naver.com>
2024-06-20 16:14:59 +0900

    랜덤데이터 더 자주 삽입하게 변경

--- web/src/main/java/web/web/admin/service/LogService.java
+++ web/src/main/java/web/web/admin/service/LogService.java
@@ -9,6 +9,8 @@ import web.web.admin.domain.log.SuccessLog;
 import web.web.admin.repository.FailureLogRepository;
 import web.web.admin.repository.SuccessLogRepository;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -34,31 +36,57 @@ public class LogService {
     }
 
     private void scheduleNextSuccessLogInsertion() {
-        int delay = ThreadLocalRandom.current().nextInt(1, 4);
-        scheduler.schedule(this::insertSuccessData, delay, TimeUnit.SECONDS);
+        scheduler.schedule(this::insertMultipleSuccessData, 1, TimeUnit.SECONDS);
     }
 
     private void scheduleNextFailureLogInsertion() {
-        int delay = ThreadLocalRandom.current().nextInt(1, 4);
-        scheduler.schedule(this::insertFailureData, delay, TimeUnit.SECONDS);
+        scheduler.schedule(this::insertMultipleFailureData, 1, TimeUnit.SECONDS);
+    }
+
+    public void insertMultipleSuccessData() {
+        int numberOfLogs = ThreadLocalRandom.current().nextInt(0, 11); // 0~10개의 로그 생성
+        for (int i = 0; i < numberOfLogs; i++) {
+            insertSuccessData();
+        }
+        scheduleNextSuccessLogInsertion(); // 다음 작업 예약
+    }
+
+    public void insertMultipleFailureData() {
+        int numberOfLogs = ThreadLocalRandom.current().nextInt(0, 11); // 0~10개의 로그 생성
+        for (int i = 0; i < numberOfLogs; i++) {
+            insertFailureData();
+        }
+        scheduleNextFailureLogInsertion(); // 다음 작업 예약
     }
 
     public void insertSuccessData() {
+        // 현재 날짜와 시간 불러오기
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        // 포맷터 정의
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        // 포맷팅
+        String formattedDateTime = currentDateTime.format(formatter);
+
         SuccessLog successLog = new SuccessLog();
-        successLog.setScenario_id(random.nextLong(3));  // 시나리오 ID를 랜덤하게 설정 (원하는 로직에 맞게 수정 가능)
-        successLog.setMessage("Random message " + random.nextInt(1000));
+        successLog.setScenario_id(random.nextLong(3)); // 시나리오 ID를 랜덤하게 설정 (원하는 로직에 맞게 수정 가능)
+        successLog.setMessage("{\"date\":\"" + formattedDateTime + "\"}");
         successLogRepository.save(successLog);
-        System.out.println("Data inserted: " + successLog.getMessage());
-        scheduleNextSuccessLogInsertion();  // 다음 작업 예약
+        System.out.println("Success Log inserted: " + successLog.getMessage());
     }
 
     public void insertFailureData() {
+        // 현재 날짜와 시간 불러오기
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        // 포맷터 정의
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        // 포맷팅
+        String formattedDateTime = currentDateTime.format(formatter);
+
         FailureLog failureLog = new FailureLog();
-        failureLog.setScenario_id(random.nextLong(3));  // 시나리오 ID를 랜덤하게 설정 (원하는 로직에 맞게 수정 가능)
-        failureLog.setMessage("Random message " + random.nextInt(1000));
+        failureLog.setScenario_id(random.nextLong(3)); // 시나리오 ID를 랜덤하게 설정 (원하는 로직에 맞게 수정 가능)
+        failureLog.setMessage("{\"date\":\"" + formattedDateTime + "\"}");
         failureLogRepository.save(failureLog);
-        System.out.println("Data inserted: " + failureLog.getMessage());
-        scheduleNextFailureLogInsertion();  // 다음 작업 예약
+        System.out.println("Failure Log inserted: " + failureLog.getMessage());
     }
 
     @PreDestroy
